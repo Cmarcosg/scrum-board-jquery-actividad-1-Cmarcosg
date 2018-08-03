d2a190651a1c452991a39c1ba3517e7487c3c0bf
Cristhian marcos <cristianmarcosgarcia@gmail.com>
2018-08-03 15:49:49 +0200

    primera modificacion

---
+++ js/app.js
@@ -0,0 +1,78 @@
+  $(document).ready(function () {
+      const generateId = namespace => `${namespace}-${Date.now()}-${Math.ceil(Math.random()*100)}`
+      const createListString = name =>
+        `<div class="list" id="${generateId('list')}">
+            <div class="listHeader">
+                <h4>${name}</h4>
+                <button>X</button>
+            </div>
+            <div class="tasks">               
+            </div>
+            <div class="addTask">
+                <input type="text">
+                <button>Add task</button>
+            </div>
+        </div>`
+
+        const createtaskstring = texto =>
+            `<div class="task" id="${generateId('task')}">
+                <div class="text">${texto}</div>
+                <button class= "eliminar">X</button>
+            </div>`
+
+      const appendNewList = (valor) => {
+          // creamos el nodo .list
+          let list = $(createListString(valor));
+
+          if (valor === '') {
+              return
+          }
+
+          // añadimos el node al DOM
+          $('.lists').append(list)
+      }
+      const appendnewtask = (event, valor) => {
+
+        // creamos el nodo .task
+        let task = $(createtaskstring(valor));
+
+        if (valor === '') {
+            return
+        }
+
+        // añadimos el node al DOM
+        $(event.target.parentNode.parentNode.querySelector('.tasks')).append(task);
+    }
+      // Listeners
+     $(document).on('keyup','.addListWrapper input', function (event) {
+        if (event.keyCode === 13) {
+              appendNewList(($(this).val()).trim());
+              $(this).val('');
+          }
+      })
+
+    //Boton listas
+    $(document).on('click', '.task button', function (event) {
+        let listnode = $(event.target);
+        listnode.detach();
+    });
+
+
+      $('.lists').on('click', '.listHeader button', function (event) {
+          let listNode = $(event.target.parentNode.parentNode);
+          listNode.detach();
+      });
+
+      $(document).on('keyup','.addTask  input', function (event) {
+        if (event.keyCode === 13) {
+            appendnewtask(event,($(this).val()).trim());
+            $(this).val('');
+        }
+    });
+    $('.lists').on('click', '.eliminar', function(event) {
+        let tarea = $(event.target.parentNode)
+        tarea.detach();
+        console.log('eliminando');
+        console.log(event.target.parentNode.parentNode);
+ })
+  })
